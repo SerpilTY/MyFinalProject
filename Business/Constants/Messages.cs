ca51b2704f0ccd6ffda7525a9bf4c8b2ee53382d
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2021-12-31 01:29:37 +0300

    10. Hafta Ders Sonu

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product Added";
+
+        public static string ProductNameInvalid = " Product Name is invalid";
+       
+        public static string ProductsListed = " Products Listed";
+
+        public static string MaintenanceTime = " System is on Maintenance"; 
+    }
+}
